3715ed2339247576d590e4fa922c5abcd439ff8f
Eric Gu <eric.yue.gu@gmail.com>
2024-05-18 18:37:46 -1000

    Replay attrs cls

--- hal/replay.py
+++ hal/replay.py
@@ -1,19 +1,39 @@
+from typing import Optional
+
 import attr
 import numpy as np
 
-from hal.types import Character
 
-
-# @attr.s(auto_attribs=True, frozen=True)
-# class PlayerState:
-#     character: np.array
-#     pos_x: np.array
-#     pos_y: np.array
-#     percent: np.array
-#     shield: np.array
-#     stock: np.array
+@attr.s(auto_attribs=True, frozen=True)
+class PlayerState:
+    """Stack player state attributes across replay frames."""
+    character: str
+    nickname: str
+    pos_x: np.ndarray[float]
+    pos_y: np.ndarray[float]
+    percent: np.ndarray[float]
+    shield: np.ndarray[float]
+    stock: np.ndarray[int]
+    facing: np.ndarray[bool]
+    action: np.ndarray[int]
+    invulnerable: np.ndarray[bool]
+    jumps_left: np.ndarray[int]
+    on_ground: np.ndarray[bool]
+    ecb_right: Optional[np.ndarray[float]] = None
+    ecb_left: Optional[np.ndarray[float]] = None
+    ecb_top: Optional[np.ndarray[float]] = None
+    ecb_bottom: Optional[np.ndarray[float]] = None
+    speed_air_x_self: Optional[np.ndarray[float]] = None
+    speed_y_self: Optional[np.ndarray[float]] = None
+    speed_x_attack: Optional[np.ndarray[float]] = None
+    speed_y_attack: Optional[np.ndarray[float]] = None
+    speed_ground_x_self: Optional[np.ndarray[float]] = None
 
 
 @attr.s(auto_attribs=True, frozen=True)
 class Replay:
-    pass
+    id: int
+    stage: str
+    frame_count: int
+    player1: PlayerState
+    player2: PlayerState
