dc27d4e109a339ee7d167c6e5f925f36184ad6fa
Eric Gu <eric.yue.gu@gmail.com>
2024-06-18 22:53:45 -0700

    Optimize setup script

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -15,13 +15,13 @@ ssh $notebook /bin/bash << EOF
   cd $REMOTE_PROJ_DIR
   if [ ! -d ".venv" ]; then
     python -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
   fi
-  source .venv/bin/activate
-  pip install -r requirements.txt
 EOF
 echo "${Yellow}Cloned hal2 repo and installed requirements"
 
 EMULATOR_DIR="/Users/ericgu/data/SSBM"
 REMOTE_EMULATOR_DIR="/opt/slippi"
-rsync -avz --delete --filter=":- .gitignore" -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR" $EMULATOR_DIR/ $notebook:$REMOTE_EMULATOR_DIR/
+rsync -avz --delete --filter=":- .gitignore" --exclude=".DS_Store" --exclude=".localized" -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR" $EMULATOR_DIR/ $notebook:$REMOTE_EMULATOR_DIR/
 echo "${Yellow}Synced emulator & iso"
