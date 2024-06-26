bcd7e177f3282fed436d2e1d3e395c08e2c88542
Eric Gu <eric.yue.gu@gmail.com>
2024-06-25 22:39:45 -0700

    Update README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Setup
+
+Development on this repo has only been tested on Ubuntu 20.04 LTS.
+
+## Set up remote machine
+
+This script copies local emulator and iso to the remote machine and sets up the environment.
+
+```shell
+scripts/setup_remote_machine.sh
+```
+
+## Downloading headless Dolphin emulator for Ubuntu 20.04 LTS
+
+[TBD] add external link to download pre-build AppImage
+
+Alternatively, you can build from source following instructions [here](https://github.com/ericyuegu/slippi-Ishiiruka/tree/ubuntu-20.04).
--- scripts/setup_remote_machine.sh
+++ scripts/setup_remote_machine.sh
@@ -2,8 +2,8 @@
 set -e
 
 Yellow='\033[0;33m'
-notebook="notebook_eric_llms"
 
+notebook="notebook_eric_llms"
 PROJECT="hal2"
 SRC_DIR="/Users/ericgu/src"
 LOCAL_PROJ_DIR="$SRC_DIR/$PROJECT"
@@ -21,13 +21,24 @@ ssh $notebook /bin/bash << EOF
 EOF
 echo "${Yellow}Cloned hal2 repo and installed requirements"
 
-EMULATOR_DIR="/Users/ericgu/data/SSBM"
+EMULATOR_DIR="/Users/ericgu/data/ssbm"
 REMOTE_EMULATOR_DIR="/opt/slippi"
 EMULATOR_FILE_NAME="ssbm_ntsc_1.02.7z"
+CISO_NAME="Super Smash Bros. Melee (USA) (En,Ja) (Rev 2).ciso"
 rsync -avz --delete --filter=":- .gitignore" --exclude=".DS_Store" --exclude=".localized" -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR" $EMULATOR_DIR/ $notebook:$REMOTE_EMULATOR_DIR/
 ssh $notebook /bin/bash << EOF
-  sudo apt-get install p7zip-full
+  sudo apt-get update
+  sudo apt-get install p7zip-full libasound2 libegl1 libgl1 libusb-1.0-0 libglib2.0-0 libgdk-pixbuf2.0-0 libpangocairo-1.0-0 -y
   cd $REMOTE_EMULATOR_DIR
   7za x $EMULATOR_FILE_NAME
+  mv $CISO_NAME ssbm.ciso
+  ./Slippi_Online-Ubuntu20.04-x86_64.AppImage --appimage-extract
 EOF
-echo "${Yellow}Synced emulator & iso"
+
+# Create a Python file with the remote paths
+cat << PYTHON_EOF > $LOCAL_PROJ_DIR/hal/emulator_paths.py
+  REMOTE_EMULATOR_PATH = "$REMOTE_EMULATOR_DIR/squashfs-root/AppRun.wrapped"
+  REMOTE_CISO_PATH = "$REMOTE_EMULATOR_DIR/ssbm.ciso"
+PYTHON_EOF
+
+echo "${Yellow}Synced emulator & iso and created emulator_paths.py"
