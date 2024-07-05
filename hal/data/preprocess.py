c1ea17aa115d9130a27816574f430a0243ad6a0d
Eric Gu <eric.yue.gu@gmail.com>
2024-07-05 15:29:17 -0700

    Calculate stats

--- hal/data/preprocess.py
+++ hal/data/preprocess.py
@@ -1,1 +1,50 @@
+import argparse
+import json
 
+import numpy as np
+import pyarrow as pa
+from loguru import logger
+from pyarrow import parquet as pq
+
+
+def calculate_statistics_for_features(input_path: str, output_path: str) -> None:
+    """Calculate and save statistics for each feature to a JSON."""
+    table: pa.Table = pq.read_table(input_path)
+    statistics = {}
+
+    for field in table.schema:
+        column = table[field.name]
+        numpy_array = column.to_numpy()
+
+        if np.issubdtype(numpy_array.dtype, np.number):
+            stats = {
+                "mean": float(np.mean(numpy_array)),
+                "std": float(np.std(numpy_array)),
+                "min": float(np.min(numpy_array)),
+                "max": float(np.max(numpy_array)),
+                "median": float(np.median(numpy_array)),
+            }
+        elif np.issubdtype(numpy_array.dtype, np.object_):
+            unique_values, counts = np.unique(numpy_array, return_counts=True)
+            stats = {
+                "unique_count": len(unique_values),
+                "top_5_values": [str(val) for val in unique_values[:5]],
+                "top_5_counts": [int(count) for count in counts[:5]],
+            }
+        else:
+            stats = {"type": str(numpy_array.dtype)}
+
+        statistics[field.name] = stats
+
+    logger.info(f"Saving statistics to {output_path}")
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(statistics, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", type=str, help="Path to the input parquet file")
+    parser.add_argument("--output_path", type=str, help="Path to the output JSON file")
+    args = parser.parse_args()
+
+    calculate_statistics_for_features(args.input_path, args.output_path)
