02eff61df24c0ce0d7fb2655f061e7a860d14cbf
Eric Gu <eric.yue.gu@gmail.com>
2024-07-04 18:18:01 -0700

    More visualizations

--- notebooks/visualize_parquet.py
+++ notebooks/visualize_parquet.py
@@ -5,17 +5,22 @@ import pyarrow as pa
 import seaborn as sns
 from pyarrow import parquet as pq
 
+from hal.data.constants import ACTION_BY_IDX
+
 np.set_printoptions(threshold=np.inf)
 
 # %%
-table: pa.Table = pq.read_table("/opt/projects/hal2/data/train.parquet")
+table: pa.Table = pq.read_table("/opt/projects/hal2/data/dev/val.parquet")
 
 # %%
 table.column_names
 
 # %%
 # randomly sample rows
-table = table.take(np.random.choice(len(table), 100000, replace=False))
+table = table.take(np.random.choice(len(table), 20000, replace=False))
+
+# %%
+table["p1_position_y"].to_numpy().max()
 
 # %%
 # uuid_filter = pc.field("replay_uuid") == 5393121284994579877
@@ -41,10 +46,10 @@ len(table)
 
 
 # %%
-def visualize_stick_heatmap(pyarrow_table: pa.Table) -> None:
+def visualize_position_heatmap(pyarrow_table: pa.Table, x_field: str, y_field: str, title: str) -> None:
     # Extract x and y values
-    x = pyarrow_table["p1_main_stick_x"].to_numpy()
-    y = pyarrow_table["p1_main_stick_y"].to_numpy()
+    x = pyarrow_table[x_field].to_numpy()
+    y = pyarrow_table[y_field].to_numpy()
 
     # Create a figure and axis
     fig, ax = plt.subplots(figsize=(10, 8))
@@ -53,13 +58,13 @@ def visualize_stick_heatmap(pyarrow_table: pa.Table) -> None:
     sns.kdeplot(x=x, y=y, cmap="YlOrRd", fill=True, cbar=True, ax=ax)
 
     # Set labels and title
-    ax.set_xlabel("Main Stick X")
-    ax.set_ylabel("Main Stick Y")
-    ax.set_title("Player 1 Main Stick Heatmap")
+    ax.set_xlabel(x_field)
+    ax.set_ylabel(y_field)
+    ax.set_title(title)
 
     # Set axis limits
-    ax.set_xlim(0, 1)
-    ax.set_ylim(1, 0)
+    ax.set_xlim(x.min(), x.max())
+    ax.set_ylim(y.min(), y.max())
 
     # Invert y-axis to match stick orientation
     ax.invert_yaxis()
@@ -69,32 +74,19 @@ def visualize_stick_heatmap(pyarrow_table: pa.Table) -> None:
 
 
 # %%
-def visualize_position_heatmap(pyarrow_table: pa.Table) -> None:
-    # Extract x and y values
-    x = pyarrow_table["p1_position_x"].to_numpy()
-    y = pyarrow_table["p1_position_y"].to_numpy()
+# plot histogram of actions
+actions = table["p1_action"].to_numpy()
+actions = [ACTION_BY_IDX[action] for action in actions]
 
-    # Create a figure and axis
-    fig, ax = plt.subplots(figsize=(10, 8))
-
-    # Create a smooth heatmap using KDE
-    sns.kdeplot(x=x, y=y, cmap="YlOrRd", fill=True, cbar=True, ax=ax)
-
-    # Set labels and title
-    ax.set_xlabel("Position X")
-    ax.set_ylabel("Position Y")
-    ax.set_title("Player 1 Position Heatmap")
-
-    # Set axis limits
-    ax.set_xlim(0, 1)
-    ax.set_ylim(1, 0)
-
-    # Invert y-axis to match stick orientation
-    ax.invert_yaxis()
-
-    # Show the plot
-    plt.show()
+# %%
+# rotate text and sort
+plt.hist(actions, bins=185, rotation=90, sort=True)
+plt.title("Player 1 Action Histogram")
+plt.show()
 
 
-# Assuming 'replay' is your data structure containing the stick values
-visualize_position_heatmap(table)
+# %%
+visualize_position_heatmap(table, "p1_position_x", "p1_position_y", "Player 1 Position Heatmap")
+
+# # %%
+visualize_position_heatmap(table, "p1_main_stick_x", "p1_main_stick_y", "Player 1 Main Stick Heatmap")
