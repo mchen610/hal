21c8c1cf4f9af50a11139bafed4ba097cbe9cdc9
Eric Gu <eric.yue.gu@gmail.com>
2024-07-04 17:01:20 -0700

    Refactor duplicate code

--- hal/data/slp_to_pyarrow.py
+++ hal/data/slp_to_pyarrow.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import multiprocessing as mp
 import random
 import sys
@@ -8,11 +9,11 @@ from typing import Dict
 from typing import Optional
 from typing import Tuple
 
-import attr
 import melee
 import pyarrow as pa
 import pyarrow.parquet as pq
 from loguru import logger
+from more_itertools import chunked
 from tqdm import tqdm
 
 from hal.data.constants import IDX_BY_ACTION
@@ -20,128 +21,79 @@ from hal.data.constants import IDX_BY_CHARACTER
 from hal.data.constants import IDX_BY_STAGE
 from hal.data.primitives import SCHEMA
 
-ControllerData = Dict[str, Any]
 FrameData = Dict[str, Any]
+ControllerData = Dict[str, Any]
 
 
 def extract_single_frame(gamestate: melee.GameState, replay_uuid: int) -> Tuple[FrameData, ControllerData]:
-    """Extracts gamestate and controller data from 1 frame of replay."""
+    """Extracts gamestate and controller data from one frame of replay."""
     players = sorted(gamestate.players.items())
     if len(players) != 2:
         raise ValueError(f"Expected 2 players, got {len(players)}")
 
-    p1_port, p1 = players[0]
-    p2_port, p2 = players[1]
-
-    # Skip pre-game frames
-    if gamestate.frame < 0:
-        return {}, {}
-
-    gamestate_frame = dict(
+    gamestate_frame: FrameData = {
         # Metadata
-        replay_uuid=replay_uuid,
-        frame=gamestate.frame,
-        # Stage
-        stage=IDX_BY_STAGE[gamestate.stage],
-        # Player 1 state
-        p1_port=p1_port,
-        p1_character=IDX_BY_CHARACTER[p1.character],
-        p1_stock=p1.stock,
-        p1_facing=int(p1.facing),
-        p1_invulnerable=int(p1.invulnerable),
-        p1_position_x=float(p1.position.x),
-        p1_position_y=float(p1.position.y),
-        p1_percent=p1.percent,
-        p1_shield_strength=p1.shield_strength,
-        p1_jumps_left=p1.jumps_left,
-        p1_action=IDX_BY_ACTION[p1.action],
-        p1_action_frame=p1.action_frame,
-        p1_invulnerability_left=p1.invulnerability_left,
-        p1_hitlag_left=p1.hitlag_left,
-        p1_hitstun_left=p1.hitstun_frames_left,
-        p1_on_ground=int(p1.on_ground),
-        p1_speed_air_x_self=p1.speed_air_x_self,
-        p1_speed_y_self=p1.speed_y_self,
-        p1_speed_x_attack=p1.speed_x_attack,
-        p1_speed_y_attack=p1.speed_y_attack,
-        p1_speed_ground_x_self=p1.speed_ground_x_self,
-        p1_ecb_bottom_x=p1.ecb_bottom[0],
-        p1_ecb_bottom_y=p1.ecb_bottom[1],
-        p1_ecb_top_x=p1.ecb_top[0],
-        p1_ecb_top_y=p1.ecb_top[1],
-        p1_ecb_left_x=p1.ecb_left[0],
-        p1_ecb_left_y=p1.ecb_left[1],
-        p1_ecb_right_x=p1.ecb_right[0],
-        p1_ecb_right_y=p1.ecb_right[1],
-        # Player 2 state
-        p2_port=p2_port,
-        p2_character=IDX_BY_CHARACTER[p2.character],
-        p2_stock=p2.stock,
-        p2_facing=int(p2.facing),
-        p2_invulnerable=int(p2.invulnerable),
-        p2_position_x=float(p2.position.x),
-        p2_position_y=float(p2.position.y),
-        p2_percent=p2.percent,
-        p2_shield_strength=p2.shield_strength,
-        p2_jumps_left=p2.jumps_left,
-        p2_action=IDX_BY_ACTION[p2.action],
-        p2_action_frame=p2.action_frame,
-        p2_invulnerability_left=p2.invulnerability_left,
-        p2_hitlag_left=p2.hitlag_left,
-        p2_hitstun_left=p2.hitstun_frames_left,
-        p2_on_ground=int(p2.on_ground),
-        p2_speed_air_x_self=p2.speed_air_x_self,
-        p2_speed_y_self=p2.speed_y_self,
-        p2_speed_x_attack=p2.speed_x_attack,
-        p2_speed_y_attack=p2.speed_y_attack,
-        p2_speed_ground_x_self=p2.speed_ground_x_self,
-        p2_ecb_bottom_x=p2.ecb_bottom[0],
-        p2_ecb_bottom_y=p2.ecb_bottom[1],
-        p2_ecb_top_x=p2.ecb_top[0],
-        p2_ecb_top_y=p2.ecb_top[1],
-        p2_ecb_left_x=p2.ecb_left[0],
-        p2_ecb_left_y=p2.ecb_left[1],
-        p2_ecb_right_x=p2.ecb_right[0],
-        p2_ecb_right_y=p2.ecb_right[1],
-    )
+        "replay_uuid": replay_uuid,
+        "frame": gamestate.frame,
+        "stage": IDX_BY_STAGE[gamestate.stage],
+    }
+    controller_frame: ControllerData = {}
 
-    p1_controller = p1.controller_state
-    p2_controller = p2.controller_state
-
-    controller_frame = dict(
-        # Player 1
-        p1_button_a=int(p1_controller.button[melee.Button.BUTTON_A]),
-        p1_button_b=int(p1_controller.button[melee.Button.BUTTON_B]),
-        p1_button_x=int(p1_controller.button[melee.Button.BUTTON_X]),
-        p1_button_y=int(p1_controller.button[melee.Button.BUTTON_Y]),
-        p1_button_z=int(p1_controller.button[melee.Button.BUTTON_Z]),
-        p1_button_start=int(p1_controller.button[melee.Button.BUTTON_START]),
-        p1_button_d_up=int(p1_controller.button[melee.Button.BUTTON_D_UP]),
-        p1_button_l=int(p1_controller.button[melee.Button.BUTTON_L]),
-        p1_button_r=int(p1_controller.button[melee.Button.BUTTON_R]),
-        p1_main_stick_x=float(p1_controller.main_stick[0]),
-        p1_main_stick_y=float(p1_controller.main_stick[1]),
-        p1_c_stick_x=float(p1_controller.c_stick[0]),
-        p1_c_stick_y=float(p1_controller.c_stick[1]),
-        p1_l_shoulder=float(p1_controller.l_shoulder),
-        p1_r_shoulder=float(p1_controller.r_shoulder),
-        # Player 2
-        p2_button_a=int(p2_controller.button[melee.Button.BUTTON_A]),
-        p2_button_b=int(p2_controller.button[melee.Button.BUTTON_B]),
-        p2_button_x=int(p2_controller.button[melee.Button.BUTTON_X]),
-        p2_button_y=int(p2_controller.button[melee.Button.BUTTON_Y]),
-        p2_button_z=int(p2_controller.button[melee.Button.BUTTON_Z]),
-        p2_button_start=int(p2_controller.button[melee.Button.BUTTON_START]),
-        p2_button_d_up=int(p2_controller.button[melee.Button.BUTTON_D_UP]),
-        p2_button_l=int(p2_controller.button[melee.Button.BUTTON_L]),
-        p2_button_r=int(p2_controller.button[melee.Button.BUTTON_R]),
-        p2_main_stick_x=float(p2_controller.main_stick[0]),
-        p2_main_stick_y=float(p2_controller.main_stick[1]),
-        p2_c_stick_x=float(p2_controller.c_stick[0]),
-        p2_c_stick_y=float(p2_controller.c_stick[1]),
-        p2_l_shoulder=float(p2_controller.l_shoulder),
-        p2_r_shoulder=float(p2_controller.r_shoulder),
-    )
+    for i, (port, player) in enumerate(players):
+        prefix = f"p{i}_"
+        gamestate_frame.update(
+            {
+                f"{prefix}port": port,
+                f"{prefix}character": IDX_BY_CHARACTER[player.character],
+                f"{prefix}stock": player.stock,
+                f"{prefix}facing": int(player.facing),
+                f"{prefix}invulnerable": int(player.invulnerable),
+                f"{prefix}position_x": float(player.position.x),
+                f"{prefix}position_y": float(player.position.y),
+                f"{prefix}percent": player.percent,
+                f"{prefix}shield_strength": player.shield_strength,
+                f"{prefix}jumps_left": player.jumps_left,
+                f"{prefix}action": IDX_BY_ACTION[player.action],
+                f"{prefix}action_frame": player.action_frame,
+                f"{prefix}invulnerability_left": player.invulnerability_left,
+                f"{prefix}hitlag_left": player.hitlag_left,
+                f"{prefix}hitstun_left": player.hitstun_frames_left,
+                f"{prefix}on_ground": int(player.on_ground),
+                f"{prefix}speed_air_x_self": player.speed_air_x_self,
+                f"{prefix}speed_y_self": player.speed_y_self,
+                f"{prefix}speed_x_attack": player.speed_x_attack,
+                f"{prefix}speed_y_attack": player.speed_y_attack,
+                f"{prefix}speed_ground_x_self": player.speed_ground_x_self,
+                f"{prefix}ecb_bottom_x": player.ecb_bottom[0],
+                f"{prefix}ecb_bottom_y": player.ecb_bottom[1],
+                f"{prefix}ecb_top_x": player.ecb_top[0],
+                f"{prefix}ecb_top_y": player.ecb_top[1],
+                f"{prefix}ecb_left_x": player.ecb_left[0],
+                f"{prefix}ecb_left_y": player.ecb_left[1],
+                f"{prefix}ecb_right_x": player.ecb_right[0],
+                f"{prefix}ecb_right_y": player.ecb_right[1],
+            }
+        )
+        controller = player.controller_state
+        controller_frame.update(
+            {
+                f"{prefix}button_a": int(controller.button[melee.Button.BUTTON_A]),
+                f"{prefix}button_b": int(controller.button[melee.Button.BUTTON_B]),
+                f"{prefix}button_x": int(controller.button[melee.Button.BUTTON_X]),
+                f"{prefix}button_y": int(controller.button[melee.Button.BUTTON_Y]),
+                f"{prefix}button_z": int(controller.button[melee.Button.BUTTON_Z]),
+                f"{prefix}button_start": int(controller.button[melee.Button.BUTTON_START]),
+                f"{prefix}button_d_up": int(controller.button[melee.Button.BUTTON_D_UP]),
+                f"{prefix}button_l": int(controller.button[melee.Button.BUTTON_L]),
+                f"{prefix}button_r": int(controller.button[melee.Button.BUTTON_R]),
+                f"{prefix}main_stick_x": float(controller.main_stick[0]),
+                f"{prefix}main_stick_y": float(controller.main_stick[1]),
+                f"{prefix}c_stick_x": float(controller.c_stick[0]),
+                f"{prefix}c_stick_y": float(controller.c_stick[1]),
+                f"{prefix}l_shoulder": float(controller.l_shoulder),
+                f"{prefix}r_shoulder": float(controller.r_shoulder),
+            }
+        )
 
     return gamestate_frame, controller_frame
 
@@ -187,29 +139,19 @@ def process_replay(replay_path: str, min_frames: int = 1500) -> Tuple[FrameData,
 
 def write_dataset_incrementally(replay_paths: Tuple[str, ...], output_path: str, batch_size: int) -> None:
     logger.info(f"Processing {len(replay_paths)} replays and writing to {Path(output_path).resolve()}")
-    batch = []
     frames_processed = 0
 
     with mp.Pool() as pool:
         data_generator = pool.imap(process_replay, replay_paths)
         with pq.ParquetWriter(output_path, schema=SCHEMA) as writer:
-            pbar = tqdm(data_generator, total=len(replay_paths), desc="Processing replays")
-            for replay_data in pbar:
-                batch.extend(replay_data)
-                frames_processed += len(replay_data)
-
-                if len(batch) >= batch_size:
-                    try:
-                        table = pa.Table.from_pylist(batch, schema=SCHEMA)
-                        writer.write_table(table)
-                    except ValueError as e:
-                        logger.error(f"Error writing batch: {e}")
-                    batch = []
-                    pbar.set_description(f"Processed {frames_processed} frames")
-
-            if batch:
-                table = pa.Table.from_pylist([attr.asdict(frame) for frame in batch], schema=SCHEMA)
-                writer.write_table(table)
+            for batch in tqdm(
+                chunked(data_generator, batch_size), total=len(replay_paths) // batch_size, desc="Processing replays"
+            ):
+                frames = list(itertools.chain.from_iterable(batch))
+                if frames:
+                    table = pa.Table.from_pylist(frames, schema=SCHEMA)
+                    writer.write_table(table)
+                    frames_processed += len(frames)
 
     logger.info(f"Finished processing. Total frames: {frames_processed}")
 
@@ -229,12 +171,12 @@ def split_train_val_test(
 
 
 def process_replays(replay_dir: str, output_dir: str, seed: int, batch_size: int, max_replays: int = -1) -> None:
-    replay_paths = list(str(path) for path in Path(replay_dir).rglob("*.slp"))
+    replay_paths = list(Path(replay_dir).rglob("*.slp"))
     if max_replays > 0:
         replay_paths = replay_paths[:max_replays]
     random.seed(seed)
     random.shuffle(replay_paths)
-    splits = split_train_val_test(input_paths=tuple(replay_paths))
+    splits = split_train_val_test(input_paths=tuple(map(str, replay_paths)))
 
     Path(output_dir).mkdir(parents=True, exist_ok=True)
 
