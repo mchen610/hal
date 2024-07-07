bbc1f0646da4b8bb21798f7853edd32387245b53
Eric Gu <eric.yue.gu@gmail.com>
2024-07-07 01:01:07 -0700

    Move files out of later_use

--- hal/later_use/embed.py
+++
@@ -1,5 +0,0 @@
-import pyarrow
-
-
-def get_replay_from_pyarrow_table():
-    pass
--- hal/training/distributed.py
+++ hal/training/distributed.py
@@ -9,9 +9,8 @@ from typing import Union
 import torch
 import torch.distributed
 import tqdm as tqdm_module
+from config import TrainerConfig
 from tqdm import tqdm
-
-from hal.later_use.config import TrainerConfig
 
 
 def barrier() -> None:
