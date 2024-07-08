1f176b522f9fe182cca1bcb3a70a897d6ffcee7a
Eric Gu <eric.yue.gu@gmail.com>
2024-07-07 21:30:08 -0700

    Refactor trainer

--- hal/training/train.py
+++ hal/training/train.py
@@ -4,15 +4,16 @@ import random
 from collections import defaultdict
 from pathlib import Path
 from time import time
-from typing import Callable
-from typing import Dict
 from typing import Iterable
 from typing import Optional
+from typing import Union
 
 import numpy as np
 import torch
+from loguru import logger
 from torch import Tensor
 from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.types import Number
 from torch.utils.data import DataLoader
 
 from hal.training.config import TrainConfig
@@ -22,6 +23,7 @@ from hal.training.dataloader import create_dataloaders
 from hal.training.distributed import auto_distribute
 from hal.training.distributed import get_world_size
 from hal.training.distributed import is_master
+from hal.training.distributed import maybe_wrap_model_distributed
 from hal.training.distributed import print
 from hal.training.distributed import trange
 from hal.training.distributed import wrap_multiprocessing
@@ -38,9 +40,7 @@ from hal.zoo.models.registry import Arch
 
 
 class Trainer(torch.nn.Module):
-    train_op: Callable[..., Dict[str, Tensor]]
-    model: torch.nn.Module
-    TRAIN_LR = (1 / 16, 1), (1 - 4 / 16, 1), (1 - 2 / 16, 0.1), (1 - 1 / 16, 0.01)
+    model: Union[torch.nn.Module, torch.nn.parallel.DistributedDataParallel]
 
     @property
     def device(self) -> str:
@@ -57,10 +57,8 @@ class Trainer(torch.nn.Module):
         self.train_loader = train_loader
         self.val_loader = val_loader
 
-        model = Arch.get(
-            config.arch, input_size=get_num_input_floats(), num_analog_values=config.num_analog_discretized_values
-        )
-        self.model = wrap_model(model)  # Needed for .backward and to wrap into a module for saving
+        model = Arch.get(self.config.arch)  # TODO input shapes
+        self.model = maybe_wrap_model_distributed(model)  # Needed for .backward and to wrap into a module for saving
         self.opt = torch.optim.AdamW(
             self.model.parameters(),
             lr=self.config.lr,
@@ -86,21 +84,71 @@ class Trainer(torch.nn.Module):
     def loss_fn(self, pred: dict[str, Tensor], target: dict[str, Tensor]) -> dict[str, Tensor]:
         raise NotImplementedError()
 
+    def train_op(self, inputs: dict[str, Tensor], targets: dict[str, Tensor]) -> dict[str, Tensor]:
+        raise NotImplementedError()
+
     def train_step(self, batch: tuple[dict[str, Tensor], dict[str, Tensor]], writer: Writer, step: int) -> None:
         batch = move_tensors_to_device(batch, self.device)
         inputs, targets = batch
-        self.opt.zero_grad(set_to_none=True)
-        pred, _ = self.model(inputs)
-        loss = self.loss_fn(pred, targets)
-        loss_total = sum(loss.values())
-        loss_total.backward()
-        self.opt.step()
-        self.scheduler.step()
+        metrics = self.train_op(inputs, targets)
+        writer.log(metrics, step=step, commit=False)
 
-        loss[LOSS_KEY] = loss_total
-        metrics_dict = {f"train/{k}": v.item() for k, v in loss.items()}
-        metrics_dict["lr/lr"] = self.scheduler.get_last_lr()
-        writer.log(metrics_dict, step=step, commit=False)
+    def train_loop(
+        self,
+        train_loader: Iterable,
+        val_loader: Iterable,
+        local_batch_size: int,
+        n_samples: int,
+        n_val_samples: int,
+        report_len: int,
+        keep_ckpts: int,
+    ) -> None:
+        logger.info(self)
+        logger.info(f"{self.artifact_dir=}")
+        assert report_len % local_batch_size == 0
+        assert n_samples % report_len == 0
+
+        wandb_config = WandbConfig.create(self, self.config) if is_master() else None
+        batch_size = get_world_size() * local_batch_size
+        train_loader = repeater(train_loader)
+
+        ckpt = Checkpoint(self, self.artifact_dir, keep_ckpts)
+        start = ckpt.restore()[0]
+
+        if start:
+            print(f"Resuming training at {start} ({start / (1 << 20):.2f}M samples)")
+
+        with Writer.create(wandb_config) as writer:
+            for i in range(start, n_samples, report_len):
+                self.train()
+                range_iter = trange(
+                    i,
+                    i + report_len,
+                    batch_size,
+                    leave=False,
+                    unit="samples",
+                    unit_scale=batch_size,
+                    desc=f"Training stage {i / report_len}/{n_samples / report_len}",
+                )
+                t0 = time()
+                for samples in range_iter:
+                    self.train_step(next(train_loader), writer=writer, step=samples)
+
+                t1 = time()
+                writer.log({"throughput/samples_per_sec_train": report_len / (t1 - t0)}, step=samples, commit=False)
+                self.validate(
+                    val_loader, batch_size=local_batch_size, n_val_samples=n_val_samples, writer=writer, step=samples
+                )
+                t2 = time()
+                writer.log({"throughput/samples_per_sec_val": n_val_samples / (t2 - t1)}, step=samples, commit=True)
+                ckpt.save(samples)
+
+                print(
+                    f"{samples / (1 << 20):.2f}M/{n_samples / (1 << 20):.2f}M samples, "
+                    f"time left {time_format((t2 - t0) * (n_samples - samples) / report_len)}"
+                )
+
+        ckpt.save_file(self.model, "model.ckpt")
 
     def val_step(self, inputs: dict[str, Tensor], targets: dict[str, Tensor]) -> dict[str, Tensor]:
         with torch.no_grad():
@@ -164,59 +212,21 @@ class Trainer(torch.nn.Module):
                 )
         writer.log(conf_matrix_dict, step=step, commit=False)
 
-    def train_loop(
-        self,
-        train_loader: Iterable,
-        val_loader: Iterable,
-        local_batch_size: int,
-        n_samples: int,
-        n_val_samples: int,
-        report_len: int,
-        keep_ckpts: int,
-    ) -> None:
-        print(self)
-        print(f"artifact_dir: {self.artifact_dir}")
-        wandb_config = WandbConfig.create(self, self.config) if is_master() else None
-        assert report_len % local_batch_size == 0
-        assert n_samples % report_len == 0
-        batch_size = get_world_size() * local_batch_size
-        train_loader = repeater(train_loader)
-        ckpt = Checkpoint(self, self.artifact_dir, keep_ckpts)
-        start = ckpt.restore()[0]
-        if start:
-            print(f"Resuming training at {start} ({start / (1 << 20):.2f}M samples)")
 
-        with Writer.create(wandb_config) as writer:
-            for i in range(start, n_samples, report_len):
-                self.train()
-                range_iter = trange(
-                    i,
-                    i + report_len,
-                    batch_size,
-                    leave=False,
-                    unit="samples",
-                    unit_scale=batch_size,
-                    desc=f"Training stage {i / report_len}/{n_samples / report_len}",
-                )
-                t0 = time()
-                for samples in range_iter:
-                    self.train_step(next(train_loader), writer=writer, step=samples)
+class RecurrentTrainer(Trainer):
+    def train_op(self, inputs: dict[str, Tensor], targets: dict[str, Tensor]) -> dict[str, Number]:
+        self.opt.zero_grad(set_to_none=True)
+        inputs, _ = self.model(inputs)
+        loss_by_head = self.loss_fn(inputs, targets)
+        loss_total: Tensor = sum(loss_by_head.values())
+        loss_total.backward()
+        self.opt.step()
+        self.scheduler.step()
 
-                t1 = time()
-                writer.log({"throughput/samples_per_sec_train": report_len / (t1 - t0)}, step=samples, commit=False)
-                self.validate(
-                    val_loader, batch_size=local_batch_size, n_val_samples=n_val_samples, writer=writer, step=samples
-                )
-                t2 = time()
-                writer.log({"throughput/samples_per_sec_val": n_val_samples / (t2 - t1)}, step=samples, commit=True)
-                ckpt.save(samples)
-
-                print(
-                    f"{samples / (1 << 20):.2f}M/{n_samples / (1 << 20):.2f}M samples, "
-                    f"time left {time_format((t2 - t0) * (n_samples - samples) / report_len)}"
-                )
-
-        ckpt.save_file(self.model, "model.ckpt")
+        loss_by_head["train/loss_total"] = loss_total
+        metrics_dict = {f"train/{k}": v.item() for k, v in loss_by_head.items()}
+        metrics_dict["lr"] = self.scheduler.get_lr()
+        return metrics_dict
 
 
 @auto_distribute
