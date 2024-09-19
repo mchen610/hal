3c922ed7c76207afb938a2e3c05383165374ee3e
Eric Gu <eric.yue.gu@gmail.com>
2024-09-19 07:36:32 -0700

    Rename notebook

--- notebooks/overfit_mlp.py
+++ notebooks/overfit_mlp.py
@@ -6,6 +6,7 @@ from tensordict import TensorDict
 
 from hal.training.io import load_model_from_artifact_dir
 
+# %%
 artifact_dir = Path(
     "/opt/projects/hal2/runs/2024-09-19_06-18-33/arch@MLPDebug-512-8_local_batch_size@32_n_samples@1048576"
 )
@@ -101,3 +102,6 @@ for i in range(1000):
     print(total_loss.item())
     total_loss.backward()
     optimizer.step()
+
+
+# %%
