f3c5cb12cf3be7d4dc263670692d3f85358e8500
Eric Gu <eric.yue.gu@gmail.com>
2024-11-03 12:32:40 -0800

    WIP refactor emulator helpers

--- hal/eval/eval_closed_loop.py
+++ hal/eval/eval_closed_loop.py
@@ -1,13 +1,9 @@
 import argparse
-import signal
 import sys
 from collections import defaultdict
 from collections import deque
-from contextlib import contextmanager
 from pathlib import Path
-from typing import Any
 from typing import DefaultDict
-from typing import Dict
 from typing import Optional
 from typing import Sequence
 
@@ -15,16 +11,15 @@ import melee
 import torch
 import torch.multiprocessing as mp
 from loguru import logger
-from melee import enums
-from melee.menuhelper import MenuHelper
 from tensordict import TensorDict
 
 from hal.data.schema import PYARROW_DTYPE_BY_COLUMN
 from hal.data.stats import load_dataset_stats
+from hal.eval.emulator_helper import console_manager
+from hal.eval.emulator_helper import get_console_kwargs
+from hal.eval.emulator_helper import self_play_menu_helper
 from hal.eval.emulator_paths import REMOTE_CISO_PATH
 from hal.eval.emulator_paths import REMOTE_DOLPHIN_HOME_PATH
-from hal.eval.emulator_paths import REMOTE_EMULATOR_PATH
-from hal.eval.emulator_paths import REMOTE_EVAL_REPLAY_DIR
 from hal.eval.eval_helper import extract_and_append_gamestate
 from hal.eval.eval_helper import send_controller_inputs
 from hal.training.io import load_model_from_artifact_dir
@@ -35,79 +30,6 @@ mp.set_start_method("spawn", force=True)
 
 PLAYER_1_PORT = 1
 PLAYER_2_PORT = 2
-
-
-def get_console_kwargs(no_gui: bool = True) -> Dict[str, Any]:
-    headless_console_kwargs = (
-        {
-            "gfx_backend": "Null",
-            "disable_audio": True,
-            "use_exi_inputs": True,
-            "enable_ffw": True,
-        }
-        if no_gui
-        else {}
-    )
-    emulator_path = REMOTE_EMULATOR_PATH
-    dolphin_home_path = REMOTE_DOLPHIN_HOME_PATH
-    Path(dolphin_home_path).mkdir(exist_ok=True, parents=True)
-    replay_dir = REMOTE_EVAL_REPLAY_DIR
-    Path(replay_dir).mkdir(exist_ok=True, parents=True)
-    console_kwargs = {
-        "path": emulator_path,
-        "is_dolphin": True,
-        "dolphin_home_path": dolphin_home_path,
-        "tmp_home_directory": False,
-        "replay_dir": replay_dir,
-        "blocking_input": True,
-        **headless_console_kwargs,
-    }
-    return console_kwargs
-
-
-def self_play_menu_helper(
-    gamestate: melee.GameState,
-    controller_1: melee.Controller,
-    controller_2: melee.Controller,
-    character_1: melee.Character,
-    character_2: melee.Character,
-    stage_selected: melee.Stage,
-) -> None:
-    if gamestate.menu_state == enums.Menu.MAIN_MENU:
-        MenuHelper.choose_versus_mode(gamestate=gamestate, controller=controller_1)
-    # If we're at the character select screen, choose our character
-    elif gamestate.menu_state == enums.Menu.CHARACTER_SELECT:
-        player_1 = gamestate.players[controller_1.port]
-        player_1_character_selected = player_1.character == character_1
-
-        if not player_1_character_selected:
-            MenuHelper.choose_character(
-                character=character_1,
-                gamestate=gamestate,
-                controller=controller_1,
-                cpu_level=0,
-                costume=0,
-                swag=False,
-                start=False,
-            )
-        else:
-            MenuHelper.choose_character(
-                character=character_2,
-                gamestate=gamestate,
-                controller=controller_2,
-                cpu_level=9,
-                costume=1,
-                swag=False,
-                start=True,
-            )
-    # If we're at the stage select screen, choose a stage
-    elif gamestate.menu_state == enums.Menu.STAGE_SELECT:
-        MenuHelper.choose_stage(
-            stage=stage_selected, gamestate=gamestate, controller=controller_1, character=character_1
-        )
-    # If we're at the postgame scores screen, spam START
-    elif gamestate.menu_state == enums.Menu.POSTGAME_SCORES:
-        MenuHelper.skip_postgame(controller=controller_1)
 
 
 def get_mock_framedata(seq_len: int) -> TensorDict:
@@ -127,24 +49,6 @@ def pad_tensors(td: TensorDict, length: int) -> TensorDict:
         pad_size = length - td.shape[0]
         return TensorDict({k: torch.nn.functional.pad(v, (pad_size, 0)) for k, v in td.items()}, batch_size=(length,))
     return td
-
-
-@contextmanager
-def console_manager(console: melee.Console):
-    def signal_handler(sig, frame):
-        raise KeyboardInterrupt
-
-    original_handler = signal.getsignal(signal.SIGINT)
-    signal.signal(signal.SIGINT, signal_handler)
-
-    try:
-        yield
-    except KeyboardInterrupt:
-        logger.info("Received interrupt, shutting down...")
-    finally:
-        signal.signal(signal.SIGINT, original_handler)
-        console.stop()
-        logger.info("Shutting down cleanly...")
 
 
 def model_server(
