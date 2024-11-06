4a1e5abc6c9e99fe79e937b543f5b7b2c9f83274
Eric Gu <eric.yue.gu@gmail.com>
2024-11-05 19:02:47 -0800

    Delete unused constants

--- hal/constants.py
+++ hal/constants.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from typing import Dict
 from typing import Final
 from typing import Literal
@@ -13,84 +12,6 @@ VALID_PLAYERS: Final[Tuple[str, str]] = ("p1", "p2")
 Player = Literal["p1", "p2"]
 PLAYER_1_PORT: Final[int] = 1
 PLAYER_2_PORT: Final[int] = 2
-
-
-# Gamestate
-STAGES: Final[Tuple[str, ...]] = (
-    "final_destination",
-    "battlefield",
-    "pokemon_stadium",
-    "dreamland",
-    "fountain_of_dreams",
-    "yoshis_story",
-)
-CHARACTERS: Final[Tuple[str, ...]] = (
-    "mario",
-    "fox",
-    "cptfalcon",
-    "dk",
-    "kirby",
-    "bowser",
-    "link",
-    "sheik",
-    "ness",
-    "peach",
-    "popo",
-    "pikachu",
-    "samus",
-    "yoshi",
-    "jigglypuff",
-    "mewtwo",
-    "luigi",
-    "marth",
-    "zelda",
-    "ylink",
-    "doc",
-    "falco",
-    "pichu",
-    "gameandwatch",
-    "ganondorf",
-    "roy",
-)
-
-
-class Character(Enum):
-    MARIO = 0x00
-    FOX = 0x01
-    CPTFALCON = 0x02
-    DK = 0x03
-    KIRBY = 0x04
-    BOWSER = 0x05
-    LINK = 0x06
-    SHEIK = 0x07
-    NESS = 0x08
-    PEACH = 0x09
-    POPO = 0x0A
-    PIKACHU = 0x0C
-    SAMUS = 0x0D
-    YOSHI = 0x0E
-    JIGGLYPUFF = 0x0F
-    MEWTWO = 0x10
-    LUIGI = 0x11
-    MARTH = 0x12
-    ZELDA = 0x13
-    YLINK = 0x14
-    DOC = 0x15
-    FALCO = 0x16
-    PICHU = 0x17
-    GAMEANDWATCH = 0x18
-    GANONDORF = 0x19
-    ROY = 0x1A
-
-
-class Stage(Enum):
-    FINAL_DESTINATION = 1
-    BATTLEFIELD = 2
-    POKEMON_STADIUM = 3
-    DREAMLAND = 4
-    FOUNTAIN_OF_DREAMS = 5
-    YOSHIS_STORY = 6
-
 
 ###################
 # Gamestate      #
