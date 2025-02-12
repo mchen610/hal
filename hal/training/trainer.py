Fix val loss_total calculation

hal/training/trainer.py
@@ -125,9 +125,7 @@ class Trainer(torch.nn.Module, abc.ABC):
     def val_op(self, batch: TensorDict) -> MetricsDict:
         with torch.no_grad():
             loss_by_head = self.forward_loop(batch)
-            loss_total = sum(v for k, v in loss_by_head.items() if k.startswith("loss"))
-
-        loss_by_head["loss_total"] = loss_total  # type: ignore
+        # We compute loss_total & averages once outside of val_op
         metrics_dict = {f"val/{k}": v.item() for k, v in loss_by_head.detach().to("cpu").items()}
         return metrics_dict
 

notebooks/find_analog_clusters.py
@@ -213,11 +213,11 @@ main_stick = main_stick[np.random.choice(len(main_stick), size=1000000, replace=
 c_stick = c_stick[np.random.choice(len(c_stick), size=1000000, replace=False)]
 
 # %%
-main_stick_10k = main_stick[np.random.choice(len(main_stick), size=10000, replace=False)]
+main_stick_1k = main_stick[np.random.choice(len(main_stick), size=1000, replace=False)]
 # %%
 # smooth heatmap of main stick using KDE
 # WARNING: SLOW
-sns.kdeplot(x=main_stick_10k[:, 0], y=main_stick_10k[:, 1], cmap="YlOrRd", fill=True, cbar=True)
+sns.kdeplot(x=main_stick_1k[:, 0], y=main_stick_1k[:, 1], cmap="YlOrRd", fill=True, cbar=True)
 plt.title("Main Stick Heatmap")
 plt.show()
 

notebooks/test_gaussian_loss.py
@@ -25,7 +25,7 @@ def plot_probs(actual_points: torch.Tensor, probs_BD: torch.Tensor, reference_po
 
 
 # %%
-loss_fn = Gaussian2DPointsLoss(torch.tensor(STICK_XY_CLUSTER_CENTERS_V0), sigma=0.08)
+loss_fn = Gaussian2DPointsLoss(torch.tensor(STICK_XY_CLUSTER_CENTERS_V0), sigma=0.1)
 
 actual_points = torch.tensor([[0.5, 0.5], [1.0, 0.5], [0.15, 0.3], [0.8338728, 0.13657573], [0.03, 0.5]])
 probs = loss_fn.transform_to_probs(actual_points)