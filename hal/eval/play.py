6af79138b098877c30f672418b444cd413f948d2
Eric Gu <eric.yue.gu@gmail.com>
2025-03-10 00:41:17 -0700

    fix shape

--- hal/eval/play.py
+++ hal/eval/play.py
@@ -56,6 +56,7 @@ def play(artifact_dir: str):
 
     mock_framedata_L: TensorDict = mock_framedata_as_tensordict(preprocessor.trajectory_sampling_len)
     context_window_BL = preprocessor.preprocess_inputs(mock_framedata_L, BOT_PLAYER).unsqueeze(0)
+    context_window_BL = preprocessor.offset_inputs(mock_framedata_L)
     context_window_BL = context_window_BL.to(device)
     logger.info(f"Context window shape: {context_window_BL.shape}, device: {context_window_BL.device}")
 
